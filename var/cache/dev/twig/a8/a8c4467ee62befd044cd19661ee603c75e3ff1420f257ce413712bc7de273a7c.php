a30e90cadba79dc499afdceab1f5645a96bac437
Romaric Destremau <romaric.Destremau@gmail.com>
2017-11-06 11:39:24 +0100

    Revert "Revert "ajout de doctrine""
    This reverts commit 20d362ceb26ab4c8ad1d995981ec5d6cc9e351db.

---
+++ var/cache/dev/twig/a8/a8c4467ee62befd044cd19661ee603c75e3ff1420f257ce413712bc7de273a7c.php
@@ -0,0 +1,66 @@
+<?php
+
+/* @WebProfiler/Icon/memory.svg */
+class __TwigTemplate_a537aef3d16665d122ba324fbc21e9ebc7fcc9c550320d46e0bbd427dc2c0416 extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = array(
+        );
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        $__internal_b40196dffca2925d73b38a1e2780b0df1122a78a8054ef70e806138e79c515c0 = $this->env->getExtension("Symfony\\Bundle\\WebProfilerBundle\\Twig\\WebProfilerExtension");
+        $__internal_b40196dffca2925d73b38a1e2780b0df1122a78a8054ef70e806138e79c515c0->enter($__internal_b40196dffca2925d73b38a1e2780b0df1122a78a8054ef70e806138e79c515c0_prof = new Twig_Profiler_Profile($this->getTemplateName(), "template", "@WebProfiler/Icon/memory.svg"));
+
+        $__internal_9c2ae6653338d64de91b3d48d1ec244b9f7d34a5aaf9af52f69a86c3a51be0a1 = $this->env->getExtension("Symfony\\Bridge\\Twig\\Extension\\ProfilerExtension");
+        $__internal_9c2ae6653338d64de91b3d48d1ec244b9f7d34a5aaf9af52f69a86c3a51be0a1->enter($__internal_9c2ae6653338d64de91b3d48d1ec244b9f7d34a5aaf9af52f69a86c3a51be0a1_prof = new Twig_Profiler_Profile($this->getTemplateName(), "template", "@WebProfiler/Icon/memory.svg"));
+
+        // line 1
+        echo "<svg version=\"1.1\" xmlns=\"http://www.w3.org/2000/svg\" x=\"0px\" y=\"0px\" width=\"24\" height=\"24\" viewBox=\"0 0 24 24\" enable-background=\"new 0 0 24 24\" xml:space=\"preserve\">
+<path fill=\"#AAAAAA\" d=\"M6,18.9V15h12v3.9c0,0.7-0.2,1.1-1,1.1H7C6.2,20,6,19.6,6,18.9z M20,1C20,1,20,1,20,1c-0.6,0-1,0.5-1,1.1
+    l0,18c0,0.5-0.4,0.9-0.9,0.9H5.9C5.4,21,5,20.6,5,20.1l0-18C5,1.5,4.6,1,4,1c0,0,0,0,0,0C3.5,1,3,1.5,3,2.1l0,18
+    C3,21.7,4.3,23,5.9,23h12.2c1.6,0,2.9-1.3,2.9-2.9l0-18C21,1.5,20.6,1,20,1z M18,9H6v5h12V9z\"/>
+</svg>
+";
+        
+        $__internal_b40196dffca2925d73b38a1e2780b0df1122a78a8054ef70e806138e79c515c0->leave($__internal_b40196dffca2925d73b38a1e2780b0df1122a78a8054ef70e806138e79c515c0_prof);
+
+        
+        $__internal_9c2ae6653338d64de91b3d48d1ec244b9f7d34a5aaf9af52f69a86c3a51be0a1->leave($__internal_9c2ae6653338d64de91b3d48d1ec244b9f7d34a5aaf9af52f69a86c3a51be0a1_prof);
+
+    }
+
+    public function getTemplateName()
+    {
+        return "@WebProfiler/Icon/memory.svg";
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  25 => 1,);
+    }
+
+    /** @deprecated since 1.27 (to be removed in 2.0). Use getSourceContext() instead */
+    public function getSource()
+    {
+        @trigger_error('The '.__METHOD__.' method is deprecated since version 1.27 and will be removed in 2.0. Use getSourceContext() instead.', E_USER_DEPRECATED);
+
+        return $this->getSourceContext()->getCode();
+    }
+
+    public function getSourceContext()
+    {
+        return new Twig_Source("<svg version=\"1.1\" xmlns=\"http://www.w3.org/2000/svg\" x=\"0px\" y=\"0px\" width=\"24\" height=\"24\" viewBox=\"0 0 24 24\" enable-background=\"new 0 0 24 24\" xml:space=\"preserve\">
+<path fill=\"#AAAAAA\" d=\"M6,18.9V15h12v3.9c0,0.7-0.2,1.1-1,1.1H7C6.2,20,6,19.6,6,18.9z M20,1C20,1,20,1,20,1c-0.6,0-1,0.5-1,1.1
+    l0,18c0,0.5-0.4,0.9-0.9,0.9H5.9C5.4,21,5,20.6,5,20.1l0-18C5,1.5,4.6,1,4,1c0,0,0,0,0,0C3.5,1,3,1.5,3,2.1l0,18
+    C3,21.7,4.3,23,5.9,23h12.2c1.6,0,2.9-1.3,2.9-2.9l0-18C21,1.5,20.6,1,20,1z M18,9H6v5h12V9z\"/>
+</svg>
+", "@WebProfiler/Icon/memory.svg", "C:\\wamp64\\www\\ProjetSymfony\\vendor\\symfony\\symfony\\src\\Symfony\\Bundle\\WebProfilerBundle\\Resources\\views\\Icon\\memory.svg");
+    }
+}
